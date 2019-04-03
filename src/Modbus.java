5b39d83182f6f8a51f013d6406fe77b01159b463
Le Duc Anh <alexdal1992@gmail.com>
2019-04-04 00:44:29 +0300

    third commit

--- src/Modbus.java
+++ src/Modbus.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 
 public class Modbus {
 
+    //Generate the hexadecimal value from the BCD code
     public static String bcdConverter(String number){
         switch(number){
             case "0000": return "0";
@@ -29,6 +30,7 @@ public class Modbus {
         }
     }
 
+    //Generate the suffix for the corresponding date
     public static String dateSuffix(String date){
         switch(date){
             case "01": case "21": case "31": return "st";
@@ -46,19 +48,25 @@ public class Modbus {
         }
     }
 
+    //Read data from file and save it to the program
     public static ArrayList readData(String filename) throws IOException, ParseException {
-
+        //Create a datalist to save all the lines in the input file
         ArrayList<String> dataList = new ArrayList<>();
         BufferedReader input = new BufferedReader(new FileReader(filename));
 
+        //Read each and every line from file and add to the datalist
         String line;
         while((line = input.readLine()) != null){
             dataList.add(line);
         }
 
+        //Extract the date from the first element of the datalist
+        //and set it as a Java's date format
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
-        dataList.set(0, "" + dateFormat.parse(dataList.get(0)));
+        dataList.set(0, dateFormat.parse(dataList.get(0)).toString());
 
+        //Remove the line numbers and the colons in each element of the datalist
+        //so that the data saved in datalist is just pure data (only number values of the registers)
         for(int i = 1; i < dataList.size(); i++){
             String[] rawData = dataList.get(i).split(":");
             dataList.set(i, rawData[1]);
@@ -67,6 +75,7 @@ public class Modbus {
         return dataList;
     }
 
+    //Fill up the bit with 0s on the left if its length is less than desired, for example, 8 or 16
     public static String fillUpBits(String number, int numberOfBits){
         while(number.length() < numberOfBits){
             number = "0" + number;
@@ -74,14 +83,16 @@ public class Modbus {
         return number;
     }
 
+    //Convert BCD to readable format
     public static String bcdToString(String number){
         String raw, value;
 
+        //Convert the string formatted integer to binary, fill it up if necessary
         raw = Integer.toBinaryString(Integer.parseInt(number));
         value = fillUpBits(raw, 16);
 
         StringBuilder bcd = new StringBuilder();
-
+        //For each 4 bits, convert to their corresponding hex value, and add them up
         for(int i = 0; i < value.length(); i+=4){
             bcd.append(bcdConverter(value.substring(i, i+4)));
         }
@@ -89,12 +100,14 @@ public class Modbus {
         return bcd.toString();
     }
 
+    //Convert BCD to readable format, with 3 registers
     public static String bcdToString(String low, String middle, String high) {
-
+        //Convert each register to hex
         String bcdLow = bcdToString(low);
         String bcdMiddle = bcdToString(middle);
         String bcdHigh = bcdToString(high);
 
+        //Swap the lower bit with the higher bit in each register
         bcdLow = bcdLow.substring(2) + bcdLow.substring(0,2);
         bcdMiddle = bcdMiddle.substring(2) + bcdMiddle.substring(0,2);
         bcdHigh = bcdHigh.substring(2) + bcdHigh.substring(0,2);
@@ -102,20 +115,29 @@ public class Modbus {
         return bcdLow + bcdMiddle + bcdHigh;
     }
 
-    public static String calendar(String number){
+    //Convert integer from register to readable integer with its respective scale given
+    public static String integerToInt(String number, int scale){
+        double value = Integer.parseInt(number);
+        return Integer.toString((int)(value * scale / 65535));
+    }
 
+    //Extract information from the date and time register, format it to readable form
+    public static String calendar(String number){
         return "20" + number.substring(10) + "-" + number.substring(8,10) + "-" + number.substring(6,8)
                 + " " + number.substring(4,6) + ":" + number.substring(2,4) + ":" + number.substring(0,2);
     }
 
+    //Extract information from the auto-save register, convert it to readable format
     public static String autosave(String number){
         return number.substring(2) + ":00 on "
                 + (number.substring(0,2).equals("00") ? "everyday" : number.substring(0,2) + dateSuffix(number.substring(0,2)));
     }
 
+    //Extract information of the signal quality
     public static String signalQuality(String number) throws NumberFormatException {
         String raw, value, quality;
 
+        //Get the lower byte and convert it to readable format
         raw = Integer.toBinaryString(Integer.parseInt(number));
         value = fillUpBits(raw, 16);
         quality = Integer.toString(Integer.parseInt(value.substring(8), 2));
@@ -123,9 +145,11 @@ public class Modbus {
         return quality;
     }
 
+    //Extract information of the working step
     public static String workingStep(String number) throws NumberFormatException {
         String raw, value, step;
 
+        //Get the higher byte and convert it to readable format
         raw = Integer.toBinaryString(Integer.parseInt(number));
         value = fillUpBits(raw, 16);
         step = Integer.toString(Integer.parseInt(value.substring(0,8), 2));
@@ -133,29 +157,64 @@ public class Modbus {
         return step;
     }
 
+    //Extract the error information
+    public static String errorCode(String number){
+        //Create a StringBuilder object to save error information available in the register
+        //and an array of all error codes
+        StringBuilder error = new StringBuilder();
+        String value = fillUpBits(Integer.toBinaryString(Integer.parseInt(number)), 16);
+
+        String[] errorCode = {"no received signal", "low received signal", "poor received signal", "pipe empty",
+                                "hardware failure", "receiving circuits gain in adjusting", "frequency at the frequency output over flow", "current at 4-20mA over flow",
+                                "RAM check-sum error", "main clock or timer clock error", "parameters check-sum error", "ROM check-sum error",
+                                "temperature circuits error", "reserved", "internal timer over flow", "analog input over range"};
+
+        //Check each bit if it indicates an error
+        //and write the error to the StringBuilder object where applicable
+        for(int i = 0; i < value.length(); i++){
+            error.append(value.charAt(i) == '1' ? errorCode[errorCode.length-1-i] + ", " : "");
+        }
+
+        //Modify the result to look better
+        if(error.length() > 0)
+            error.delete(error.length() - 2, error.length());
+        else
+            error.append("No error");
+
+        return error.toString();
+    }
+
+    //Convert long format to readable integer format
     public static int longToInt(String low, String high) throws NumberFormatException {
         String number, original, firstCompliment = "";
         int sign, value;
 
+        //Get the high and low byes and combine them together under binary form
         high = Integer.toBinaryString(Integer.parseInt(high));
         low = Integer.toBinaryString(Integer.parseInt(low));
         number = fillUpBits(high,16) + fillUpBits(low,16);
 
-        sign = (int)Math.pow(-1, Integer.parseInt("" + number.charAt(0)));
+        //Determine the sign of the number and extract the value part from its sign
+        sign = (int)Math.pow(-1, Integer.parseInt(Character.toString(number.charAt(0))));
         original = number.substring(1);
 
+        //If sign is positive, convert it to integer
         if (sign == 1) {
             value = Integer.parseInt(original, 2);
+        //If sign is negative, perform the 2's compliment conversion
         }else {
+            //Swap the value of each bit in the binary string
             for (int i = 0; i < original.length(); i++) {
                 firstCompliment += (original.charAt(i) == '0' ? '1' : '0');
             }
+            //Convert to integer and plus 1
             value = Integer.parseInt(firstCompliment, 2) + 1;
         }
 
         return sign * value;
     }
 
+    //Convert real4 to readable floating-point decimal format
     public static double real4ToDouble(String low, String high) throws NumberFormatException {
         //form a single number from both high and low values of the registers
         //and extract it into 3 parts: sign, mantissa, exponent
@@ -165,7 +224,7 @@ public class Modbus {
 
         //Convert both high and low register values into binary values,
         //fill up the binary values up to 16 bits
-        // add add them up in order: high first, low second
+        //add add them up in order: high first, low second
         high = Integer.toBinaryString(Integer.parseInt(high));
         low = Integer.toBinaryString(Integer.parseInt(low));
         number = fillUpBits(high,16) + fillUpBits(low,16);
@@ -232,22 +291,21 @@ public class Modbus {
         System.out.println("Current input at AI5: " + real4ToDouble(dataList.get(47), dataList.get(48)) + " mA");
         System.out.println();
 
-        //System password at 49-50 registers
+        //System password at 49-50 registers unsolved
 
         System.out.println("Password for hardware: " + bcdToString(dataList.get(51)));
         System.out.println("Calendar (date and time): " + calendar(bcdToString(dataList.get(53),dataList.get(54),dataList.get(55))));
         System.out.println("Day+hour for auto-save: " + autosave(bcdToString(dataList.get(56))));
         System.out.println();
 
-        System.out.println("Key to input: " + Integer.parseInt(dataList.get(59)));
-        System.out.println("Go to Window #: " + Integer.parseInt(dataList.get(60)));
-        System.out.println("LCD back-lit lights: " + Integer.parseInt(dataList.get(61)) + " seconds");
-        System.out.println("Times for the beeper: " + Integer.parseInt(dataList.get(62)));
-        System.out.println("Pulses left for OCT: " + Integer.parseInt(dataList.get(63)));
+        System.out.println("Key to input: " + dataList.get(59));
+        System.out.println("Go to Window #: " + dataList.get(60));
+        System.out.println("LCD back-lit lights: " + dataList.get(61) + " seconds");
+        System.out.println("Times for the beeper: " + dataList.get(62));
+        System.out.println("Pulses left for OCT: " + dataList.get(63));
         System.out.println();
 
-        //Error code at 72 register
-
+        System.out.println("Error Code: " + errorCode(dataList.get(72)));
         System.out.println("PT100 resistance of inlet: " + real4ToDouble(dataList.get(77), dataList.get(78)) + " Ohm");
         System.out.println("PT100 resistance of outlet: " + real4ToDouble(dataList.get(79), dataList.get(80)) + " Ohm");
         System.out.println();
@@ -261,8 +319,8 @@ public class Modbus {
         System.out.println("Output current: " + real4ToDouble(dataList.get(89), dataList.get(90)) + " mA");
         System.out.println("Working step: " + workingStep(dataList.get(92)));
         System.out.println("Signal quality: " + signalQuality(dataList.get(92)));
-        System.out.println("Upstream strength: " + Integer.parseInt(dataList.get(93)));     //needs modification
-        System.out.println("Downstream strength: " + Integer.parseInt(dataList.get(94)));   //needs modification
+        System.out.println("Upstream strength: " + integerToInt(dataList.get(93), 2047));
+        System.out.println("Downstream strength: " + integerToInt(dataList.get(94), 2047));
         System.out.println();
 
         System.out.println("Language used in user interface: " + language(dataList.get(96)));
